893a16da90e4ae424d96813ab714cfdb1ac01c1b
hyphnd <james.donguez01@gmail.com>
2025-02-15 22:16:27 +0800

    messed up

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 
+use App\Models\Info;
 use App\Http\Controllers\GreetController;
+use App\Http\Controllers\InfoController;
 
+
+Route::get('/', function(){
+    return view('Ello');
+});
 Route::get('/hello', function () {
     return 'Hello me!';
 });
 
 Route::get('/greet', [GreetController::class, 'greet']);
+
+Route::resource('Info', InfoController::class);
